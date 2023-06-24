ac2fd1a082160123b42c9bd79f6a44cff5e8ac6d
atropos <sv7n@pm.me>
2023-06-24 14:16:04 +0100

    Moved to different logger based approach

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="atro-pylog",
-    version="0.0.8",
+    version="0.0.9",
     packages=find_packages(),
     author="Atropos",
     author_email="pypi.rising@atro.xyz",
