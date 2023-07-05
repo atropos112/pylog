98df638bb4c9f377e7f026da22a2e55a7ffa4b0a
atropos <sv7n@pm.me>
2023-07-05 07:57:26 +0100

    moved opentel to http

--- pylog/opentelemetry_setup.py
+++ pylog/opentelemetry_setup.py
@@ -1,6 +1,6 @@
 from opentelemetry import trace
 from opentelemetry._logs import set_logger_provider
-from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
+from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
